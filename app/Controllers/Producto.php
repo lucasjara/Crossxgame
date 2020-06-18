c4a66f693538dc01f8330b220f24248f493261da
MrJakess <jaque8982@gmail.com>
2020-06-17 20:07:15 -0400

    vista producto agregada

---
+++ app/Controllers/Producto.php
@@ -0,0 +1,9 @@
+<?php namespace App\Controllers;
+
+class Producto extends BaseController
+{
+    public function index()
+    {
+        return $this->vista('producto/vista');
+    }
+}
---
+++ app/Views/producto/vista.php
@@ -0,0 +1,80 @@
+<?php ?>
+
+<section class="product-section">
+		<div class="container">
+			<div class="back-link">
+				<a href="./category.html"> &lt;&lt; Back to Category</a>
+			</div>
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="product-pic-zoom">
+						<img class="product-big-img" src="img/single-product/p1.jpg" alt="">
+					</div>
+					<div class="product-thumbs" tabindex="1" style="overflow: hidden; outline: none;">
+						<!--<div class="product-thumbs-track">
+							<div class="pt active" data-imgbigurl="img/single-product/p1.jpg"><img src="img/single-product/thumb-p1.jpg" alt=""></div>
+							<div class="pt" data-imgbigurl="img/single-product/p2.jpg"><img src="img/single-product/thumb-p2.jpg" alt=""></div>
+							<div class="pt" data-imgbigurl="img/single-product/p3.jpg"><img src="img/single-product/thumb-p3.jpg" alt=""></div>
+							<div class="pt" data-imgbigurl="img/single-product/p4.jpg"><img src="img/single-product/thumb-p4.jpg" alt=""></div>
+						</div>-->
+					</div>
+				</div>
+				<div class="col-lg-6 product-details">
+					<h1 class="p-title">Funko Pop</h1>
+					<h2 class="p-price">$45.000</h2>
+					<h4 class="p-stock">Disponibilidad: <span>Sin Stock</span></h4>
+
+				   		<!--<div class="p-rating">
+						<i class="fa fa-star-o"></i>
+						<i class="fa fa-star-o"></i>
+						<i class="fa fa-star-o"></i>
+						<i class="fa fa-star-o"></i>
+						<i class="fa fa-star-o fa-fade"></i>
+					</div>
+					<div class="p-review">
+						<a href="">3 reviews</a>|<a href="">Add your review</a>
+					</div>-->
+				
+					<div class="quantity">
+						<p>Cantidad</p>
+                        <div class="pro-qty"> <input type="text" value="1"></div>
+                    </div>
+					<a href="#" class="site-btn">COMPRAR</a>
+					<div id="accordion" class="accordion-area">
+						<div class="panel">
+							<div class="panel-header" id="headingOne">
+								<button class="panel-link active" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">information</button>
+							</div>
+							<div id="collapse1" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra tempor so dales. Phasellus sagittis auctor gravida. Integer bibendum sodales arcu id te mpus. Ut consectetur lacus leo, non scelerisque nulla euismod nec.</p>
+									<p>Approx length 66cm/26" (Based on a UK size 8 sample)</p>
+									<p>Mixed fibres</p>
+									<p>The Model wears a UK size 8/ EU size 36/ US size 4 and her height is 5'8"</p>
+								</div>
+							</div>
+						</div>
+						<div class="panel">
+							<div class="panel-header" id="headingTwo">
+								<button class="panel-link" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">care details </button>
+							</div>
+							<div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+								<div class="panel-body">
+									<img src="./img/cards.png" alt="">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra tempor so dales. Phasellus sagittis auctor gravida. Integer bibendum sodales arcu id te mpus. Ut consectetur lacus leo, non scelerisque nulla euismod nec.</p>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+					<div class="social-sharing">
+						<a href=""><i class="fa fa-google-plus"></i></a>
+						<a href=""><i class="fa fa-pinterest"></i></a>
+						<a href=""><i class="fa fa-facebook"></i></a>
+						<a href=""><i class="fa fa-twitter"></i></a>
+						<a href=""><i class="fa fa-youtube"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>

76192caa5e168e6928c03e27fdf0e862ca79a23a
ManuelGodoy51 <manuelalejandrogodoyvergara@gmail.com>
2020-06-17 20:42:13 -0400

    renombre de las id de vista registro

--- app/Views/registro/vista.php
+++ app/Views/registro/vista.php
@@ -6,65 +6,65 @@
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      <input id="txtEmail" type="email" class="form-control" placeholder="Email">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Rut</label>
-      <input type="text" class="form-control" id="inputPassword4" placeholder="11111111-1">
+      <input id="txtRut" type="text" class="form-control" placeholder="11111111-1">
     </div>
   </div>
     <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Nombre</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Ingrese su Nombre...">
+      <input id="txtNombre" type="email" class="form-control" placeholder="Ingrese su Nombre...">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Apellido</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Ingrese su Apellido...">
+      <input id="txtApellido" type="password" class="form-control" placeholder="Ingrese su Apellido...">
     </div>
   </div>
    <div class="form-group">
     <label for="inputAddress">Region</label>
-    <select id="inputState" class="form-control">
+    <select id="selectRegion" class="form-control">
         <option selected="">Seleccione Region...</option>
         <option>...</option>
       </select>
   </div>
   <div class="form-group">
     <label for="inputAddress">Ciudad</label>
-    <select id="inputState" class="form-control">
+    <select id="selectCiudad" class="form-control">
         <option selected="">Seleccione Ciudad...</option>
         <option>...</option>
       </select>
   </div>
   <div class="form-group">
     <label for="inputAddress">Direccion</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Ingrese su Direccion">
+    <input id="txtDireccion" type="text" class="form-control" placeholder="Ingrese su Direccion">
   </div>
       <div class="form-group">
       <label for="inputAddress2">Fecha de nacimiento</label>
-      <input type="text" class="form-control" id="inputCity" placeholder="00/00/0000">
+      <input id="txtFechaNacimiento" type="text" class="form-control" placeholder="00/00/0000">
     </div>
   <div class="form-row">
   	    <div class="form-group col-md-6">
       <label for="inputPassword4">Contraseña</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Contraseña">
+      <input id="txtContraseña" type="password" class="form-control" placeholder="Contraseña">
     </div>
         <div class="form-group col-md-6">
       <label for="inputPassword4">Repita su Contraseña</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Contraseña">
+      <input id="txtRepetirContraseña" type="password" class="form-control" placeholder="Contraseña">
     </div>
   </div>
   <div class="form-group">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <input class="form-check-input" type="checkbox" id="gchTerminos">
       <label class="form-check-label" for="gridCheck">
         Acepto los terminos y condiciones..
       </label>
     </div>
-    <div style="margin-top: 3%;transform: translateX(33%);">
-  		<button style="margin-bottom: 3%;" type="submit" class="btn btn-primary">Registrar</button>
-  		<button style="margin-bottom: 3%" type="submit" class="btn btn-secundary">limpiar</button>
+    <div style="margin-top: 3%;transform: translateX(39%);">
+  		<button style="margin-bottom: 3%;" type="submit" class="btn btn-primary" id="btnRegistrar">Registrar</button>
+  		<button style="margin-bottom: 3%" type="submit" class="btn btn-secundary" id="btnLimpiar">limpiar</button>
   </div>
   </div>
 </form>

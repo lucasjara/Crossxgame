ce77a73968c29f7d9d79a354acd07a490fc58e82
MrJakess <jaque8982@gmail.com>
2020-06-17 21:04:34 -0400

    no message

--- app/Views/contacto/vista.php
+++ app/Views/contacto/vista.php
@@ -23,6 +23,6 @@
 				</div>
 			</div>
 		</div>
-		<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3112.131123014736!2d-72.59223178465437!3d-38.73775317959563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9614d3dd74dbaf25%3A0x1ddf9e1e607204e2!2sArturo%20Prat%20439%2C%20Temuco%2C%20Araucan%C3%ADa!5e0!3m2!1sen!2scl!4v1592440509878!5m2!1sen!2scl" style="border:0" allowfullscreen></iframe></div>
+		<div class="map"> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3112.131123014736!2d-72.59223178465437!3d-38.73775317959563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9614d3dd74dbaf25%3A0x1ddf9e1e607204e2!2sArturo%20Prat%20439%2C%20Temuco%2C%20Araucan%C3%ADa!5e0!3m2!1sen!2scl!4v1592440509878!5m2!1sen!2scl" style="border:0" allowfullscreen></iframe></div>
 	</section>
 	<br>	<!-- Contact section end -->

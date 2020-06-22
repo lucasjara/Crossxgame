645ff0f82c2b1857c8e8adf7c6347cd265d5b02a
ManuelGodoy51 <manuelalejandrogodoyvergara@gmail.com>
2020-06-21 23:18:16 -0400

    actualizacion modal

--- app/Views/master/head.php
+++ app/Views/master/head.php
@@ -23,10 +23,8 @@
     <link rel="stylesheet" href="public/crossxgame/css/owl.carousel.min.css"/>
     <link rel="stylesheet" href="public/crossxgame/css/animate.css"/>
     <link rel="stylesheet" href="public/crossxgame/css/style.css"/>
-    <!-- Stylesheets modal -->
-    
 
-      
+    <!-- JS modal -->  
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
@@ -86,7 +84,7 @@
                               </div>  
                                 <div class="up-item">
                                     <i class="breadcrumb-item">
-                                    <a href="#">Crear cuenta </a></i>
+                                    <a href="/Crossxgame/public/registro">Crear cuenta </a></i>
                                 </div>
 
                             </form> 

8f0fc2821235caa75c201dffe4899bfb90161822
MrJakess <jaque8982@gmail.com>
2020-07-13 13:56:38 -0400

    correccion de footer y de pagina categoria

--- app/Views/categoria/vista.php
+++ app/Views/categoria/vista.php
@@ -24,7 +24,7 @@
 						<h2 class="fw-title">Ordenar</h2>
 						<div class="price-range-wrap" >
 							<h4>Precio</h4>
-                            <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content" data-min="0" data-max="39990">
+                            <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content" data-min="0" data-max="499990">
 								<div class="ui-slider-range ui-corner-all ui-widget-header" style="left: 0%; width: 100%;"></div>
 								<span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: 0%;">
 								</span>
--- app/Views/master/footer.php
+++ app/Views/master/footer.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 
 <!--====== Javascripts & Jquery ======-->
@@ -16,74 +15,47 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-lg-3 col-sm-6">
+            <div class="col-6 col-md-4">
                 <div class="footer-widget about-widget">
-                    <h2>About</h2>
+                    <h2>CrossXgame</h2>
                     <p>Donec vitae purus nunc. Morbi faucibus erat sit amet congue mattis. Nullam frin-gilla faucibus urna, id dapibus erat iaculis ut. Integer ac sem.</p>
-                    <img src="public/crossxgame/img/cards.png" alt="">
+                    <!--<img src="public/crossxgame/img/cards.png" alt="">-->
                 </div>
             </div>
-            <div class="col-lg-3 col-sm-6">
+            <div class="col-6 col-md-4">
                 <div class="footer-widget about-widget">
-                    <h2>Questions</h2>
+                    <h2>HACERCA DE NOSOTROS</h2>
                     <ul>
-                        <li><a href="">About Us</a></li>
-                        <li><a href="">Track Orders</a></li>
-                        <li><a href="">Returns</a></li>
-                        <li><a href="">Jobs</a></li>
-                        <li><a href="">Shipping</a></li>
-                        <li><a href="">Blog</a></li>
+ 
+                        <li><a href="">inicio</a></li>
+                        <li><a href="">Servicios tecnicos</a></li>
+                        <li><a href="">Compras</a></li>
                     </ul>
                     <ul>
                         <li><a href="">Partners</a></li>
-                        <li><a href="">Bloggers</a></li>
-                        <li><a href="">Support</a></li>
-                        <li><a href="">Terms of Use</a></li>
-                        <li><a href="">Press</a></li>
+                        <li><a href="">Soporte</a></li>
+                        <li><a href="">Terminos de uso</a></li>
                     </ul>
                 </div>
             </div>
-            <!--<div class="col-lg-3 col-sm-6">
-                <div class="footer-widget about-widget">
-                    <h2>Questions</h2>
-                    <div class="fw-latest-post-widget">
-                        <div class="lp-item">
-                            <div class="lp-thumb set-bg" data-setbg="img/blog-thumbs/1.jpg"></div>
-                            <div class="lp-content">
-                                <h6>what shoes to wear</h6>
-                                <span>Oct 21, 2018</span>
-                                <a href="#" class="readmore">Read More</a>
-                            </div>
-                        </div>
-                        <div class="lp-item">
-                            <div class="lp-thumb set-bg" data-setbg="img/blog-thumbs/2.jpg"></div>
-                            <div class="lp-content">
-                                <h6>trends this year</h6>
-                                <span>Oct 21, 2018</span>
-                                <a href="#" class="readmore">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>-->
-            <div class="col-lg-3 col-sm-6">
+            <div class="col-6 col-md-4">
                 <div class="footer-widget contact-widget">
-                    <h2>Questions</h2>
+                    <h2>CONTACTANOS</h2>
                     <div class="con-info">
                         <span>C.</span>
-                        <p>Your Company Ltd </p>
+                        <p>CrossXgame Ltd </p>
                     </div>
                     <div class="con-info">
-                        <span>B.</span>
-                        <p>1481 Creekside Lane  Avila Beach, CA 93424, P.O. BOX 68 </p>
+                        <span>D.</span>
+                        <p>Arturo Prat, 439, Galeria Massmann,Local 3 </p>
                     </div>
                     <div class="con-info">
                         <span>T.</span>
-                        <p>+53 345 7953 32453</p>
+                        <p>+00 111 111 111</p>
                     </div>
                     <div class="con-info">
                         <span>E.</span>
-                        <p>office@youremail.com</p>
+                        <p>Contacto@Crossxgame.com</p>
                     </div>
                 </div>
             </div>
@@ -99,7 +71,7 @@
             </div>
 
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            <p class="text-white text-center mt-5">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+            <p class="text-white text-center ">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 
         </div>

a39105a29e5b1c933c78621fbb5f3e95498f756e
MrJakess <jaque8982@gmail.com>
2020-07-27 23:11:22 -0400

    no message

--- app/Views/clientecuenta/vista.php
+++ app/Views/clientecuenta/vista.php
@@ -16,18 +16,17 @@
 					
 				<div class="col-lg-12 order- order-lg-1">
 					<form class="checkout-form">
-						<div class="cf-title"><h2 style="color: white;">Mi cuenta</h2></div>
-
-						
+						<div class="cf-title"><h2 style="color: white;">Mi cuenta</h2><a ><h4 style="color: white;">Editar</h4></a></div>
 						<div class="row col-md-7">
 							<div class="col-md-12 payment-list">
-								<li>Rut: <label>11.111.111-1</label></li>
-								<li>Nombre: <label>juan unito</label></li>
-								<li>Apellido: <label>peres pepito</label></li>
-								<li>ciudad: <label>temuco</label></li>
-								<li>Direccion: <label>calle falza 123</label></li>
-								<li>correo: <label>juanunitoperespepito@gmail.com</label></li>
-								<li>Numero celular: <label>+569 9999999</label></li>
+								<li>Rut: <label id="rut" ></label></li>
+								<li>Nombre: <label id="rut" >juan unito</label></li>
+								<li>Apellido: <label id="rut" >peres pepito</label></li>
+								<li>ciudad: <label id="rut" >temuco</label></li>
+								<li>Direccion: <label id="rut" >calle falza 123</label></li>
+								<li>correo: <label id="rut" >juanunitoperespepito@gmail.com</label></li>
+								<li>Fecha Nacimiento: <label id="rut" >1995-05-25</label></li>
+								<li>Numero celular: <label id="rut" >+569 9999999</label></li>
 													
 							</div>
 						</div>

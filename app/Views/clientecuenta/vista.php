3a7bd2e37f011a4f7b7e11d00e60d9e84f0746a3
ManuelGodoy51 <manuelalejandrogodoyvergara@gmail.com>
2020-07-28 13:39:38 -0400

    Modificación vista clientecuenta

--- app/Views/clientecuenta/vista.php
+++ app/Views/clientecuenta/vista.php
@@ -1,4 +1,66 @@
-
+<!-----------------------------------------Modal--------------------------------------------------->
+<form class="contact-form" id="miFormActualizacion">
+    <div class="modal fade" id="Modalmodificación" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modificación de datos</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <form>
+                            <div class="form-group">
+                                <label for="inputPassword4">Nombre</label>
+                                <input id="txtContraseñaModal" name="contraseñaLogin" type="password" class="form-control" placeholder="Contraseña">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword4">Apellido</label>
+                                <input id="txtContraseñaModal" name="contraseñaLogin" type="password" class="form-control" placeholder="Contraseña">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail4">Rut</label>
+                                <input id="txtEmailModal" name="emailLogin" type="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                            <label for="inputAddress" >Region</label>
+                            <select id="selectRegion" onchange="ShowSelected();" class="form-control" style="border-radius: 1em">
+                                <option value="0" selected>Seleccione Region...</option>
+                                <?php
+                                //print_r($arrProfesiones);
+                                //foreach ($arrProfesiones as $i => $region_nombre)
+                                  //  echo '<option values="',$i,'">',$region_nombre,'</option>';
+                                // ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Comuna</label>
+                            <select id="selectComuna" class="form-control" style="border-radius: 1em">
+                                <option selected="">Seleccione Comuna...</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                                <label for="inputEmail4">Dirección</label>
+                                <input id="txtEmailModal" name="emailLogin" type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                                <label for="inputEmail4">Email</label>
+                                <input id="txtEmailModal" name="emailLogin" type="email" class="form-control" placeholder="Email">
+                        </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="button" id="btnModificar" name="btnModificar" class="btn btn-primary">Modificar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+<!-----------------------------------------Modal Final--------------------------------------------->
 	<!-- checkout section  -->
 	<br>
 		<br>
@@ -18,7 +80,7 @@
 								<li>Direccion: <label><?php echo $direccion?></label></li>
 								<li>correo: <label><?php echo $email ?></label></li>
 								<li>Fecha de Nacimiento: <label><?php echo $f_nacimiento?></label></li>
-													
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Modalmodificación">Primary</button>			
 							</div>
 						</div>
 						<div class="cf-title">Mis Pedidos</div>

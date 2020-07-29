37d9f58104723e188be5b17e9a087086aeb21a31
MrJakess <jaque8982@gmail.com>
2020-07-28 23:18:58 -0400

    no message

--- app/Views/error/vista.php
+++ app/Views/error/vista.php
@@ -7,7 +7,7 @@
 <br>
 	<br>
 <div class="alert alert-danger" role="alert">
-  <h4 class="alert-heading">UPS!</h4>
+  <h4 class="alert-heading">UPS!</h4> <img style="justify-content: center;" src="/Crossxgame/public/public/crossxgame/img/F.gif" alt="..." class="img-thumbnail">
   <p>Error 404, La página que buscas no existe o no esta disponible en este momento.</p>
   <hr>
   <p class="mb-0">Clickea <a href="/Crossxgame/public/prueba">Aquí</a> para volver a la Página Principal.</p>

54f0f33c86773064ae224a6561c3148d461c2bbe
MrJakess <jaque8982@gmail.com>
2020-07-29 03:33:06 -0400

    correccion footer

--- app/Views/master/footer_administracion.php
+++ app/Views/master/footer_administracion.php
@@ -12,13 +12,19 @@
 
 </body>
 <section class="footer-section navbar-dark sticky-top bg-dark flex-md-nowrap p-0" >
+<div class="social-links-warp">
+        <div class="container">
+            <div class="social-links">
+            
+                
+            </div>
 
-           
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            <p class="text-white text-center mt-5">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
+            <p class="text-white text-center ">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-<br>
 
+        </div>
+    </div>
 </section>
 
 
